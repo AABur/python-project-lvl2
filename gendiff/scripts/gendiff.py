24df27fc1c1762d92c2e4f4f37cd028ecfc0f782
AABur <aabur@mail.ru>
2020-10-11 03:38:26 +0300

    refactor: refactoring

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -12,6 +12,15 @@ diff = generate_diff(file_path1, file_path2)
 """
 import argparse
 import json
+import os
+
+import yaml
+
+LOADER = {  # noqa:WPS407
+    '.json': json.load,
+    '.yaml': yaml.safe_load,
+    '.yml': yaml.safe_load,
+}
 
 
 def main():
@@ -24,11 +33,11 @@ def main():
     parser.add_argument('second_file', type=argparse.FileType('r'))
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    diff = (generate_diff(args.first_file.name, args.second_file.name))
-    print(diff.replace(';', '\n'))
+    diff = (generate_diff(args.first_file.name, args.second_file.name, 'str'))
+    print(diff)
 
 
-def generate_diff(first_file_path, second_file_path):
+def generate_diff(first_file_path, second_file_path, output_format='str'):
     """Generate diff function.
 
     Compares two files.
@@ -36,47 +45,60 @@ def generate_diff(first_file_path, second_file_path):
     Args:
         first_file_path (str): path to first file for comparison
         second_file_path (str): path to second file for comparison
+        output_format (str, optional): output format. Defaults to 'str'.
 
     Returns:
-        (str): string with differences
+        (str): output_format='str'
+        (dic): {key:{status}}
     """
-    combined_data = combine_data(
-        collect_data_from_file(first_file_path),
-        collect_data_from_file(second_file_path),
-    )
-    difference = ''
-    for key in combined_data.keys():
-        val1, val2 = combined_data[key]
-        if val1 == val2:
-            difference += '  {}: {};'.format(key, val1)
-        elif not val2:
-            difference += '- {}: {};'.format(key, val1)
-        elif not val1:
-            difference += '+ {}: {};'.format(key, val2)
-        else:
-            difference += '- {}: {};'.format(key, val1)
-            difference += '+ {}: {};'.format(key, val2)
-    return difference[:-1]
+    first = collect_from_file(first_file_path)
+    second = collect_from_file(second_file_path)
+    compared = compare(first, second)
+    if output_format == 'str':
+        return str(compared)
+    return compared
 
 
-def combine_data(first_data, second_data):
-    """Combine two dictionaries.
+def compare(first, second):
+    """[summary].
+
+    [extended_summary]
 
     Args:
-        first_data (dict): first dictionary {key1: value1}
-        second_data (dict): second dictionary {key2: value2}
+        first ([type]): [description]
+        second ([type]): [description]
 
     Returns:
-        dict: combined dictionary {key: [value1/None, value2/None]}
+        [type]: [description]
     """
-    combined_data = {}
-    keys = sorted(first_data.keys() | second_data.keys())
-    for key in keys:
-        combined_data[key] = [first_data.get(key), second_data.get(key)]
-    return combined_data
+    compared = {}
+    for key in sorted(first.keys() | second.keys()):
+        if key in first.keys() - second.keys():
+            compared[key] = {
+                'status': 'removed',
+                'value': first.get(key),
+            }
+        if key in second.keys() - first.keys():
+            compared[key] = {
+                'status': 'added',
+                'value': second.get(key),
+            }
+        if key in first.keys() & second.keys():
+            if first.get(key) == second.get(key):
+                compared[key] = {
+                    'status': 'unchanged',
+                    'value': first.get(key),
+                }
+            else:
+                compared[key] = {
+                    'status': 'updated',
+                    'old_value': first.get(key),
+                    'new_value': second.get(key),
+                }
+    return compared
 
 
-def collect_data_from_file(file_path):
+def collect_from_file(file_path):
     """Collect data from json file.
 
     Args:
@@ -86,9 +108,33 @@ def collect_data_from_file(file_path):
         dict: collected data
     """
     with open(file_path) as data_file:
-        collected_data = json.load(data_file)
+        _, ext = os.path.splitext(file_path)
+        collected = LOADER.get(ext)(data_file)
         data_file.close()
-    return collected_data
+    return collected
+
+
+def flatten(nested):
+    """[summary].
+
+    [extended_summary]
+
+    Args:
+        nested (dict): [description]
+
+    Returns:
+        (dict): [description]
+    """
+    flatted = {}
+    for k1, v1 in nested.items():
+        if isinstance(v1, dict):
+            if not v1:
+                flatted[k1] = ''
+            for k2, v2 in flatten(v1).items():
+                flatted['{0}/{1}'.format(k1, k2)] = v2
+        else:
+            flatted[k1] = v1
+    return flatted
 
 
 if __name__ == '__main__':
