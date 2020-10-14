6f7389181382c2fdf9d496f868d2a5e75d9b2733
AABur <aabur@mail.ru>
2020-10-15 00:32:43 +0300

    test: updetated tests regarding updated data structure

--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -8,7 +8,7 @@ first_json = 'gendiff/tests/fixtures/file1.json'
 second_json = 'gendiff/tests/fixtures/file2.json'
 first_yaml = 'gendiff/tests/fixtures/file1.yaml'
 second_yaml = 'gendiff/tests/fixtures/file2.yaml'
-control_value = "{'follow': {'status': 'removed', 'value': False}, 'host': {'status': 'unchanged', 'value': 'hexlet.io'}, 'proxy': {'status': 'removed', 'value': '123.234.53.22'}, 'timeout': {'status': 'updated', 'old_value': 50, 'new_value': 20}, 'verbose': {'status': 'added', 'value': True}}"  # noqa:E501
+control_value = "{'follow': {'status': 'removed', 'value': False}, 'host': {'status': 'unchanged', 'value': 'hexlet.io'}, 'proxy': {'status': 'removed', 'value': '123.234.53.22'}, 'timeout': {'status': 'updated', 'value': 50, 'updated_value': 20}, 'verbose': {'status': 'added', 'value': True}}"  # noqa:E501
 
 
 def test_generate_diff():
