7872df73ea505e66be00d1d5b453bce8126d52b3
AABur <aabur@mail.ru>
2020-10-18 23:13:54 +0300

    feat: output format

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -11,13 +11,14 @@ from gendiff import generate_diff
 diff = generate_diff(file_path1, file_path2)
 """
 import argparse
+import json
 
 from gendiff.comparator import generate_diff
 from gendiff.format_json import render
 from gendiff.format_plain import renderer
 
 
-def main():
+def main():  # noqa:WPS213
     """Generate diff CLI.
 
     CLI usage: gendiff [-h] [-f FORMAT] first_file second_file
@@ -28,9 +29,12 @@ def main():
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
     diff = (generate_diff(args.first_file.name, args.second_file.name, 'dict'))
+    print('structured')
     print(render(diff))
-    print('-----------')
+    print('plain')
     print(renderer(diff))
+    print('json')
+    print(json.dumps(diff))
 
 
 if __name__ == '__main__':
