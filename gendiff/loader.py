ebbd0dbb5610653ebccb9a53db3b8825be50b44d
AABur <aabur@mail.ru>
2020-10-20 02:52:54 +0300

    feat: check extension and rise error

--- gendiff/loader.py
+++ gendiff/loader.py
@@ -3,38 +3,32 @@
 
 import json
 import os
+from os import error
 
 import yaml
-
-
-def get_loader(loader):
-    """Call data loader.
-
-    Call the loader according to the file extension
-
-    Args:
-        loader (str): file extention
-
-    Returns:
-        fucnction: file loader
-    """
-    return {
-        '.json': json.load,
-        '.yaml': yaml.safe_load,
-        '.yml': yaml.safe_load,
-    }.get(loader)
 
 
 def collect_data(file_path):
     """Collect data from file.
 
     Args:
-        file_path (str): path to JSON or YAML file
+        file_path(str): path to JSON or YAML file
+
+    Raises:
+        error: [description]
 
     Returns:
         (dict): decoded data
     """
+    loaders = {
+        '.json': json.load,
+        '.yaml': yaml.safe_load,
+        '.yml': yaml.safe_load,
+    }
+    call_loader = loaders.get
+    correct_exts = loaders.keys()
+    _, ext = os.path.splitext(file_path)
+    if ext not in correct_exts:
+        raise error('Wrong file')
     with open(file_path) as data_file:
-        _, ext = os.path.splitext(file_path)
-        collected = get_loader(ext)(data_file)
-    return collected
+        return call_loader(ext)(data_file)
