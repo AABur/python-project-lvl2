50d47a24eeed82cada1f07f2412d54c864fe2b53
AABur <aabur@mail.ru>
2020-10-23 18:11:09 +0300

    test: complex data plain test

--- gendiff/tests/test_format_plain.py
+++ gendiff/tests/test_format_plain.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
 """Test plain format generator."""
-
+import pytest
 
 from gendiff.formaters.format_plain import print_plain
 
-test_data = {
+simple_data = {
     'follow':
         {
             'status': 'removed',
@@ -33,7 +33,7 @@ test_data = {
         },
 }
 
-test_result = ''.join(
+simple_result = ''.join(
     (
         ' Property follow was removed \n',
         ' Property proxy was removed \n',
@@ -42,6 +42,46 @@ test_result = ''.join(
     ),
 )
 
+complex_data = {
+    'group1': {
+        'baz': {
+            'status': 'updated',
+            'value': 'bas',
+            'updated_value': 'bars',
+        },
+        'foo': {
+            'status': 'unchanged',
+            'value': 'bar',
+        },
+        'nest': {
+            'status': 'updated',
+            'value': {'key': 'value'},
+            'updated_value': 'str',
+        },
+    },
+    'group2': {
+        'status': 'removed',
+        'value': {'abc': 12345, 'deep': {'id': 45}},
+    },
+    'group3': {
+        'status': 'added',
+        'value': {'fee': 100500, 'deep': {'id': {'number': 45}}},
+    },
+}
 
-def test_print_plain():
+complex_result = ''.join(
+    (
+        " Property group1.baz was updated. From 'bas' to 'bars' \n",
+        " Property group1.nest was updated. From '[complex value]' to 'str' \n",
+        ' Property group2 was removed \n',
+        " Property group3 was added with value: '[complex value]' \n",
+    ),
+)
+
+
+@pytest.mark.parametrize(('test_data', 'test_result'), [
+    (simple_data, simple_result),
+    (complex_data, complex_result),
+])
+def test_print_plain(test_data, test_result):
     assert print_plain(test_data) == test_result
