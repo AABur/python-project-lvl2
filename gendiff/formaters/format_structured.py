b927b74720d1a78d7c6aa856c94c82ba537b4f0b
AABur <aabur@mail.ru>
2020-10-25 19:49:47 +0200

    refactor: reduce complexity for structure dformatter

--- gendiff/formaters/format_structured.py
+++ gendiff/formaters/format_structured.py
@@ -29,11 +29,11 @@ def render(source, indent=0):  # noqa:WPS231
     if isinstance(source, dict):
         output_items = []
         for key in source:
+            new_indent = indent + INDENT_STEP
             source_value = source.get(key)
             status = source_value.get(STATUS, EMPTY) if isinstance(
                 source_value, dict,
             ) else EMPTY
-            new_indent = indent + INDENT_STEP
             if status == UPDATED:
                 output_items.append(
                     new_item(
@@ -51,8 +51,16 @@ def render(source, indent=0):  # noqa:WPS231
                         new_indent,
                     ),
                 )
-                continue
-            if status != EMPTY:
+            elif status == EMPTY:
+                output_items.append(
+                    new_item(
+                        key,
+                        status,
+                        source_value,
+                        new_indent,
+                    ),
+                )
+            else:
                 output_items.append(
                     new_item(
                         key,
@@ -61,15 +69,6 @@ def render(source, indent=0):  # noqa:WPS231
                         new_indent,
                     ),
                 )
-                continue
-            output_items.append(
-                new_item(
-                    key,
-                    status,
-                    source_value,
-                    new_indent,
-                ),
-            )
         return '{{{0}}}'.format(
             EMPTY.join(output_items) + LF_CH + HT_CH * indent,
         )
