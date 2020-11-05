24ede109f5dbbb1f26e8302c2512a49a4e4b5f14
AABur <aabur@mail.ru>
2020-11-05 02:30:42 +0200

    refactor:

--- gendiff/comparator.py
+++ gendiff/comparator.py
@@ -33,24 +33,21 @@ def compare(old, new):
     keys = {
         'keys_union': sorted(old.keys() | new.keys()),
         'keys_removed': old.keys() - new.keys(),
-        'keys_added': new.keys() - old.keys(),
         'keys_intersection': old.keys() & new.keys(),
     }
     for key in keys['keys_union']:
         old_value = old.get(key)
         new_value = new.get(key)
-        if key in keys['keys_removed']:
-            compared[key] = {
-                STATUS: REMOVED,
-                VALUE: old_value,
-            }
-        if key in keys['keys_added']:
-            compared[key] = {
-                STATUS: ADDED,
-                VALUE: new_value,
-            }
         if key in keys['keys_intersection']:
             compared[key] = compare_same_keys(old_value, new_value)
+            continue
+        compared[key] = {
+            STATUS: REMOVED,
+            VALUE: old_value,
+        } if key in keys['keys_removed'] else {
+            STATUS: ADDED,
+            VALUE: new_value,
+        }
     return compared
 
 
