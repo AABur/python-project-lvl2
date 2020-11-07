64d8614edd59206a6135c69b21de255d675c1ffa
AABur <aabur@mail.ru>
2020-11-07 22:29:08 +0200

    test: change the location and names of the test files

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,50 +5,50 @@ import pytest
 
 
 @pytest.fixture()
-def simple_before():
-    return 'tests/fixtures/simple_before.yaml'
+def simple_before_path():
+    return 'tests/fixtures/simple/before.yaml'
 
 
 @pytest.fixture()
-def simple_after():
-    return 'tests/fixtures/simple_after.yaml'
+def simple_after_path():
+    return 'tests/fixtures/simple/after.yaml'
 
 
 @pytest.fixture()
-def complex_before():
-    return 'tests/fixtures/complex_before.json'
+def complex_before_path():
+    return 'tests/fixtures/complex/before.json'
 
 
 @pytest.fixture()
-def complex_after():
-    return 'tests/fixtures/complex_after.json'
+def complex_after_path():
+    return 'tests/fixtures/complex/after.json'
 
 
 @pytest.fixture()
 def result_complex_json():
-    return 'tests/fixtures/result_complex_json.txt'
+    return 'tests/fixtures/complex/result_json.txt'
 
 
 @pytest.fixture()
 def result_complex_plain():
-    return 'tests/fixtures/result_complex_plain.txt'
+    return 'tests/fixtures/complex/result_plain.txt'
 
 
 @pytest.fixture()
 def result_complex_structured():
-    return 'tests/fixtures/result_complex_structured.txt'
+    return 'tests/fixtures/complex/result_structured.txt'
 
 
 @pytest.fixture()
 def result_simple_json():
-    return 'tests/fixtures/result_simple_json.txt'
+    return 'tests/fixtures/simple/result_json.txt'
 
 
 @pytest.fixture()
 def result_simple_plain():
-    return 'tests/fixtures/result_simple_plain.txt'
+    return 'tests/fixtures/simple/result_plain.txt'
 
 
 @pytest.fixture()
 def result_simple_structured():
-    return 'tests/fixtures/result_simple_structured.txt'
+    return 'tests/fixtures/simple/result_structured.txt'
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -9,12 +9,12 @@ from gendiff.comparator import generate_diff
 @pytest.mark.parametrize(
     'style, file_before, fiel_after, file_result',
     [
-        ('plain', 'simple_before', 'simple_after', 'result_simple_plain'),
-        ('plain', 'complex_before', 'complex_after', 'result_complex_plain'),
-        ('structured', 'simple_before', 'simple_after', 'result_simple_structured'),  # noqa:E501
-        ('structured', 'complex_before', 'complex_after', 'result_complex_structured'),  # noqa:E501
-        ('json', 'simple_before', 'simple_after', 'result_simple_json'),
-        ('json', 'complex_before', 'complex_after', 'result_complex_json'),
+        ('plain', 'simple_before_path', 'simple_after_path', 'result_simple_plain'),
+        ('plain', 'complex_before_path', 'complex_after_path', 'result_complex_plain'),
+        ('structured', 'simple_before_path', 'simple_after_path', 'result_simple_structured'),  # noqa:E501
+        ('structured', 'complex_before_path', 'complex_after_path', 'result_complex_structured'),  # noqa:E501
+        ('json', 'simple_before_path', 'simple_after_path', 'result_simple_json'),
+        ('json', 'complex_before_path', 'complex_after_path', 'result_complex_json'),
     ],
 )
 def test_generate_diff(style, file_before, fiel_after, file_result, request):
