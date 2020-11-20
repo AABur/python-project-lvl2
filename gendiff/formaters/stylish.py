51b71f4e248eed559b32adbe8e2f9b9cda5fa71f
AABur <aabur@mail.ru>
2020-11-20 22:28:24 +0200

    refactor: stylich

--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -15,7 +15,7 @@ HT_CH = ' '
 INDENT_STEP = 4
 
 
-def prepare_stylish(source, indent=0):
+def prepare_stylish(source, indent=0):  # noqa:WPS210
     if isinstance(source, bool):
         return str(source).lower()
     if source is None:
@@ -25,50 +25,27 @@ def prepare_stylish(source, indent=0):
     output_items = []
     for key, value in source.items():
         status = value.get(STATUS) if isinstance(value, dict) else None
+        new_indent = indent + INDENT_STEP
         if status == UPDATED:
-            output_items.extend(get_updated(
-                key,
-                value,
-                indent + INDENT_STEP,
-            ))
+            output_items.extend(get_updated(key, value, new_indent))
             continue
-        output_items.extend(create_item(
-            key,
-            status,
-            value.get(VALUE) if status else value,
-            indent + INDENT_STEP,
-        ))
-    return '{{{0}}}'.format(
-        ''.join(output_items) + LF_CH + HT_CH * indent,
-    )
+        new_value = value.get(VALUE) if status else value
+        output_items.extend(create_item(key, status, new_value, new_indent))
+    return '{{{0}}}'.format(''.join(output_items) + LF_CH + HT_CH * indent)
 
 
-def get_updated(key, source_value, new_indent):
-    item_before = create_item(
-        key,
-        REMOVED,
-        source_value.get(VALUE),
-        new_indent,
-    )
-    item_after = create_item(
-        key,
-        ADDED,
-        source_value.get(UPDATED_VALUE),
-        new_indent,
-    )
+def get_updated(key, source, indent):
+    item_before = create_item(key, REMOVED, source.get(VALUE), indent)
+    item_after = create_item(key, ADDED, source.get(UPDATED_VALUE), indent)
     return item_before, item_after
 
 
-def create_item(key, status, new_value, indent):
+def create_item(key, status, value, indent):
     prefix = LF_CH + HT_CH * indent
     status_sign = get_status_sign(status)
     if status_sign:
         prefix = prefix[:-2] + status_sign
-    return '{0}{1}: {2}'.format(
-        prefix,
-        key,
-        prepare_stylish(new_value, indent),
-    )
+    return '{0}{1}: {2}'.format(prefix, key, prepare_stylish(value, indent))
 
 
 def get_status_sign(status):
