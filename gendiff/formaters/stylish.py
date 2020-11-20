df5ff2bc1ad61ea58978f4ac8239530ace84a804
AABur <aabur@mail.ru>
2020-11-20 21:45:33 +0200

    refactor: stylish

--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -13,41 +13,34 @@ from gendiff.comparator import (
 LF_CH = '\n'
 HT_CH = ' '
 INDENT_STEP = 4
-EMPTY = ''
 
 
 def prepare_stylish(source, indent=0):
-    if isinstance(source, dict):
-        output_items = []
-        for key in source:
-            source_value = source.get(key)
-            status = get_status(source_value)
-            if status == UPDATED:
-                output_items.extend(get_updated(
-                    key,
-                    source_value,
-                    indent + INDENT_STEP,
-                ))
-                continue
-            value = source_value if status == EMPTY else source_value.get(VALUE)
-            output_items.extend(create_item(
+    if isinstance(source, bool):
+        return str(source).lower()
+    if source is None:
+        return 'null'
+    if not isinstance(source, dict):
+        return source
+    output_items = []
+    for key, value in source.items():
+        status = value.get(STATUS) if isinstance(value, dict) else None
+        if status == UPDATED:
+            output_items.extend(get_updated(
                 key,
-                status,
                 value,
                 indent + INDENT_STEP,
             ))
-        return '{{{0}}}'.format(
-            EMPTY.join(output_items) + LF_CH + HT_CH * indent,
-        )
-    return format_value(source)
-
-
-def format_value(value):
-    if isinstance(value, bool):
-        return str(value).lower()
-    if value is None:
-        return 'null'
-    return value
+            continue
+        output_items.extend(create_item(
+            key,
+            status,
+            value.get(VALUE) if status else value,
+            indent + INDENT_STEP,
+        ))
+    return '{{{0}}}'.format(
+        ''.join(output_items) + LF_CH + HT_CH * indent,
+    )
 
 
 def get_updated(key, source_value, new_indent):
@@ -66,22 +59,20 @@ def get_updated(key, source_value, new_indent):
     return item_before, item_after
 
 
-def get_status(source_value):
-    return source_value.get(STATUS, EMPTY) if isinstance(
-        source_value, dict,
-    ) else EMPTY
-
-
 def create_item(key, status, new_value, indent):
-    get_status_sign = {
-        ADDED: '+ ',
-        REMOVED: '- ',
-    }.get(status)
     prefix = LF_CH + HT_CH * indent
-    if get_status_sign:
-        prefix = prefix[:-2] + get_status_sign
+    status_sign = get_status_sign(status)
+    if status_sign:
+        prefix = prefix[:-2] + status_sign
     return '{0}{1}: {2}'.format(
         prefix,
         key,
         prepare_stylish(new_value, indent),
     )
+
+
+def get_status_sign(status):
+    return {
+        ADDED: '+ ',
+        REMOVED: '- ',
+    }.get(status)
