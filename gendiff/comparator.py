6a9ce6af1baaaabb0fae3ee2c4a82fb3ef489a56
AABur <aabur@mail.ru>
2020-11-22 08:46:44 +0200

    refactor: refactor diff generator

--- gendiff/comparator.py
+++ gendiff/comparator.py
@@ -12,37 +12,25 @@ VALUE = 'value'
 UPDATED_VALUE = 'updated_value'
 
 
-def compile_diff(old, new):  # noqa:WPS210
+def compile_diff(old, new):
     compared = {}
     keys_union = sorted(old.keys() | new.keys())
     keys_removed = old.keys() - new.keys()
+    # keys_added = new.keys() - old.keys() # noqa:E800
     keys_intersection = old.keys() & new.keys()
     for key in keys_union:
-        old_value = old.get(key)
-        new_value = new.get(key)
         if key in keys_intersection:
-            compared[key] = compare_same_keys(old_value, new_value)
-            continue
-        compared[key] = {
-            STATUS: REMOVED,
-            VALUE: old_value,
-        } if key in keys_removed else {
-            STATUS: ADDED,
-            VALUE: new_value,
-        }
+            compared[key] = compare_same_keys(old.get(key), new.get(key))
+        elif key in keys_removed:
+            compared[key] = {STATUS: REMOVED, VALUE: old.get(key)}
+        else:  # key in keys_added
+            compared[key] = {STATUS: ADDED, VALUE: new.get(key)}
     return compared
 
 
 def compare_same_keys(old_value, new_value):
-    if old_value == new_value:
-        return {
-            STATUS: UNCHANGED,
-            VALUE: old_value,
-        }
-    elif isinstance(old_value, dict) and isinstance(new_value, dict):
+    if isinstance(old_value, dict) and isinstance(new_value, dict):
         return compile_diff(old_value, new_value)
-    return {
-        STATUS: UPDATED,
-        VALUE: old_value,
-        UPDATED_VALUE: new_value,
-    }
+    if old_value == new_value:
+        return {STATUS: UNCHANGED, VALUE: old_value}
+    return {STATUS: UPDATED, VALUE: old_value, UPDATED_VALUE: new_value}
