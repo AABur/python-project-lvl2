846a4eadb9f804fe813d2b094fa19cb1093bc494
AABur <aabur@mail.ru>
2020-11-26 22:12:35 +0200

    refactor: refactoring plain formatter

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -13,12 +13,12 @@ from gendiff.comparator import (
 
 COMPLEX_VALUE = '[complex value]'
 
-
 ADDED_STR = "Property '{0}' was added with value: {1}"
 REMOVED_STR = "Property '{0}' was removed"
 UPDATED_STR = "Property '{0}' was updated. From {1} to {2}"
 
 
+# FIXME Found too many local variables: 7 > 5 flake8(WPS210)
 def prepare_plain(diff):  # noqa: WPS210
     get_pattern = {
         ADDED: ADDED_STR,
@@ -26,7 +26,7 @@ def prepare_plain(diff):  # noqa: WPS210
         UPDATED: UPDATED_STR,
     }.get
     plain_diff = []
-    sorted_diff = sort_diff(flatt(diff))
+    sorted_diff = sort_diff(flatten(diff, '', {}))
     for key, value in sorted_diff.items():
         pattern = get_pattern(value.get(STATUS))
         if pattern:
@@ -42,16 +42,14 @@ def sort_diff(diff):
     return dict(sorted(diff.items(), key=lambda item: item[0]))
 
 
-def flatt(nest):  # noqa: WPS210
-    flatted = {}
-    for nest_key, nest_value in nest.items():
-        if nest_value.get(STATUS):
-            flatted[nest_key] = nest_value
-        else:
-            for child_key, child_value in flatt(nest_value).items():
-                flatted_key = '{0}.{1}'.format(nest_key, child_key)
-                flatted[flatted_key] = child_value
-    return flatted
+def flatten(nest, key, result):
+    if nest.get(STATUS):
+        result[key] = nest
+    else:
+        for next_key in nest:
+            new_key = '{0}.{1}'.format(key, next_key) if key else next_key
+            flatten(nest[next_key], new_key, result)
+    return result
 
 
 def format_value(value):
