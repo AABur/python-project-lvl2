441715f52a545fc236bac97cbbf3c250a68358ac
AABur <aabur@mail.ru>
2020-12-04 20:16:56 +0200

    refactor: change exception handling

--- gendiff/loader.py
+++ gendiff/loader.py
@@ -12,22 +12,34 @@ from yaml.scanner import ScannerError
 EXT_ERROR_MSG = '\n{0} - invalid file extension\nuse one of: .json .yaml .yml'
 YAML_ERROR_MSG = '\n{0} - incorrect YAML file'
 JSON_ERROR_MSG = '\n{0} - incorrect JSON file'
+OS_ERROR_MSG = '\n{0} - file access error'
 
 
+class GendiffError(Exception):
+    pass
+
+
+# TODO error massage and traceback
 def collect_data(file_path):
     _, ext = os.path.splitext(file_path)
-    loader = {
-        '.json': json.load,
-        '.yaml': yaml.safe_load,
-        '.yml': yaml.safe_load,
-    }.get(ext.lower())
+    loader = _call_loader(ext)
     sys.tracebacklimit = 0
     if not loader:
         raise RuntimeError(EXT_ERROR_MSG.format(file_path))
     try:
         with open(file_path) as data_file:
             return loader(data_file)
-    except ScannerError as exc:
-        raise RuntimeError(YAML_ERROR_MSG.format(file_path)) from exc
-    except JSONDecodeError as exc:
-        raise RuntimeError(JSON_ERROR_MSG.format(file_path)) from exc
+    except ScannerError:
+        raise GendiffError(YAML_ERROR_MSG.format(file_path))
+    except JSONDecodeError:
+        raise GendiffError(JSON_ERROR_MSG.format(file_path))
+    except OSError:
+        raise GendiffError(OS_ERROR_MSG.format(file_path))
+
+
+def _call_loader(ext):
+    return {
+        '.json': json.load,
+        '.yaml': yaml.safe_load,
+        '.yml': yaml.safe_load,
+    }.get(ext.lower())
