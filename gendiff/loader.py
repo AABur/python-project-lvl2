32d7750eddc93e54eb9ed74480262d052db93dbb
AABur <aabur@mail.ru>
2020-12-13 14:49:42 +0100

    refactor: change file load error massages

--- gendiff/loader.py
+++ gendiff/loader.py
@@ -8,10 +8,10 @@ from json.decoder import JSONDecodeError
 import yaml
 from yaml.scanner import ScannerError
 
-EXT_ERROR_MSG = '\n{0} - invalid file extension\nuse one of: .json .yaml .yml'
-YAML_ERROR_MSG = '\n{0} - incorrect YAML file'
-JSON_ERROR_MSG = '\n{0} - incorrect JSON file'
-OS_ERROR_MSG = '\n{0} - file access error'
+EXT_ERROR_MSG = '{0} - invalid file extension\nuse one of: .json .yaml .yml'
+YAML_ERROR_MSG = '{0} - incorrect YAML file'
+JSON_ERROR_MSG = '{0} - incorrect JSON file'
+OS_ERROR_MSG = '{0} - file access error'
 
 
 call_loader = {
@@ -30,13 +30,13 @@ def collect_data(file_path):
     _, ext = os.path.splitext(file_path)
     loader = call_loader(ext.lower())
     if not loader:
-        raise RuntimeError(EXT_ERROR_MSG.format(file_path))
+        raise GendiffError(EXT_ERROR_MSG.format(file_path))
     try:
         with open(file_path) as data_file:
             return loader(data_file)
-    except ScannerError:
-        raise GendiffError(YAML_ERROR_MSG.format(file_path))
-    except JSONDecodeError:
-        raise GendiffError(JSON_ERROR_MSG.format(file_path))
-    except OSError:
-        raise GendiffError(OS_ERROR_MSG.format(file_path))
+    except ScannerError as err:
+        raise GendiffError(err, YAML_ERROR_MSG.format(file_path))
+    except JSONDecodeError as err:
+        raise GendiffError(err, JSON_ERROR_MSG.format(file_path))
+    except OSError as err:
+        raise GendiffError(err, OS_ERROR_MSG.format(file_path))
