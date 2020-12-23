e36075ddb5fd8d39a9586a1cc729d03b38e9dbe9
AABur <aabur@mail.ru>
2020-12-23 01:14:06 +0100

    test: test incorrect files

---
+++ tests/test_loader.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+"""Generate diff tests."""
+
+import pytest
+
+from gendiff.loader import GendiffFileError, collect_data
+
+
+@pytest.mark.parametrize(
+    'file_path',
+    [
+        ('tests/fixtures/wrong_ext.ttt'),
+        ('tests/fixtures/wrong_json.json'),
+        ('tests/fixtures/wrong_yaml.yaml'),
+        ('file_no_exists'),
+    ],
+)
+def test_wrong_file(file_path):
+    with pytest.raises(GendiffFileError):
+        assert collect_data(file_path)
