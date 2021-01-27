38fff8ebc389aa1ec711f0baa6380f2d9cf02527
AABur <aabur@mail.ru>
2021-01-27 10:00:37 +0200

    refactor: split formatting and diff bypass

--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -25,33 +25,47 @@ def format_stylish(diff):
     return prepare_stylish(sort_diff(diff))
 
 
+# FIXME
 def prepare_stylish(node, indent=0):  # noqa: WPS210
-    if not isinstance(node, dict):
-        return format_value(node)
     output = []
     for node_key, node_value in node.items():
         new_indent = indent + INDENT_STEP
-        status = node_value.get(STATUS) if isinstance(node_value, dict) else None  # noqa: E501
+        status = node_value.get(STATUS)
         if status == UPDATED:
-            value = prepare_stylish(node_value[VALUE], new_indent)
+            value = format_value(node_value[VALUE], new_indent)
             output.append(create_item(new_indent, REMOVED, node_key, value))
-            value = prepare_stylish(node_value[UPDATED_VALUE], new_indent)
+            value = format_value(node_value[UPDATED_VALUE], new_indent)
             output.append(create_item(new_indent, ADDED, node_key, value))
             continue
-        if status is None:
-            value = prepare_stylish(node_value, new_indent)
-        else:
+        if status == NESTED:
             value = prepare_stylish(node_value[VALUE], new_indent)
+        else:
+            value = format_value(node_value[VALUE], new_indent)
         output.append(create_item(new_indent, status, node_key, value))
     return '{{{0}}}'.format(''.join(output) + LF_CHAR + INDENT_CHAR * indent)
 
 
-def format_value(value):
+def format_value(value, indent):
+    if isinstance(value, dict):
+        return format_dict(value, indent)
     if isinstance(value, bool):
         return str(value).lower()
     if value is None:
         return 'null'
-    return value
+    return str(value)
+
+
+def format_dict(node, indent=0):  # noqa: WPS210
+    output = []
+    for node_key, node_value in node.items():
+        new_indent = indent + INDENT_STEP
+        status = node_value.get(STATUS) if isinstance(node_value, dict) else None  # noqa: E501
+        if status is None:
+            value = format_value(node_value, new_indent)
+        else:
+            value = format_value(node_value[VALUE], new_indent)
+        output.append(create_item(new_indent, status, node_key, value))
+    return '{{{0}}}'.format(''.join(output) + LF_CHAR + INDENT_CHAR * indent)
 
 
 def create_item(indent, status, node_key, node_value):
